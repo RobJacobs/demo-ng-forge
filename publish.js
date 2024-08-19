0142f398ed201b61637c58760c3133b221d9272a
RobJacobs <rob.jacobs@tylertech.com>
2024-08-19 11:01:09 -0400

    chore: fix publish

--- publish.js
+++ publish.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 
+fs.cpSync('dist/demo-ng-forge', 'docs', { recursive: true });
+
 fs.copyFile('docs/index.html', 'docs/404.html', (err) => {
     if (err) {
-        console.error('index.html -> 404.html copy failed:');
+        console.error('index.html -> 404.html copy failed');
         throw err;
     }
 });
