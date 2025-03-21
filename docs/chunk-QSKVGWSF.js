aa41a68e8f2cd8cf39b6978ddcd2e20f405160c4
RobJacobs <rob.jacobs@tylertech.com>
2025-03-21 10:49:17 -0400

    chore: publish demo

---
+++ docs/chunk-QSKVGWSF.js
@@ -0,0 +1,2 @@
+import{ea as o}from"./chunk-5VCAQWVT.js";var n=new o("PetsService"),p=[{path:"",loadComponent:()=>import("./chunk-M4LAX4XP.js").then(t=>t.PetsMainComponent)}];export{n as a,p as b};
+//# sourceMappingURL=chunk-QSKVGWSF.js.map
