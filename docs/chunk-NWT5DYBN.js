be88b7ed404a6defbd6ef7d638ce141a3217d2db
RobJacobs <rob.jacobs@tylertech.com>
2025-06-10 13:54:05 -0400

    chore: publish demo

---
+++ docs/chunk-NWT5DYBN.js
@@ -0,0 +1,2 @@
+import{jb as p}from"./chunk-BPFPFXGP.js";var o=(()=>{let t=class t{transform(i,e,...a){return e(i,...a)}};t.\u0275fac=function(e){return new(e||t)},t.\u0275pipe=p({name:"appCallback",type:t,pure:!0});let r=t;return r})();export{o as a};
+//# sourceMappingURL=chunk-NWT5DYBN.js.map
---
+++ docs/chunk-XRZHNNDL.js
@@ -0,0 +1,2 @@
+import{a,b}from"./chunk-TBBXV5KB.js";import"./chunk-BPFPFXGP.js";import"./chunk-OY7QI42D.js";export{b as PETS_ROUTES,a as PETS_SERVICE};
+//# sourceMappingURL=chunk-XRZHNNDL.js.map
