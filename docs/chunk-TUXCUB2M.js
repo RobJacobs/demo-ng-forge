b1f82cb51eb8600dca7988983df83d35374ef54e
RobJacobs <rob.jacobs@tylertech.com>
2025-08-19 14:06:49 -0400

    chore: publish demo

---
+++ docs/chunk-TUXCUB2M.js
@@ -0,0 +1,2 @@
+import{kb as p}from"./chunk-MOKV6GK3.js";var o=(()=>{let t=class t{transform(i,e,...a){return e(i,...a)}};t.\u0275fac=function(e){return new(e||t)},t.\u0275pipe=p({name:"appCallback",type:t,pure:!0});let r=t;return r})();export{o as a};
+//# sourceMappingURL=chunk-TUXCUB2M.js.map
