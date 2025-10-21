f82fff7172354d5e0be0780f6f8a0f3c1aecbd1a
RobJacobs <rob.jacobs@tylertech.com>
2025-10-21 13:29:32 -0400

    chore: publish demo

---
+++ docs/chunk-KG5XDX5I.js
@@ -0,0 +1,2 @@
+import{a,b}from"./chunk-RW4JM45J.js";import"./chunk-LKW6WE5H.js";import"./chunk-OY7QI42D.js";export{b as PETS_ROUTES,a as PETS_SERVICE};
+//# sourceMappingURL=chunk-KG5XDX5I.js.map
---
+++ docs/chunk-RW4JM45J.js
@@ -0,0 +1,2 @@
+import{ga as o}from"./chunk-LKW6WE5H.js";var n=new o("PetsService"),p=[{path:"",loadComponent:()=>import("./chunk-YNBWOBIU.js").then(t=>t.PetsMainComponent)}];export{n as a,p as b};
+//# sourceMappingURL=chunk-RW4JM45J.js.map
