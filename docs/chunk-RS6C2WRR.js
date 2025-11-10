584b4eb37b4a4d50f830d1c72818bd3fe8315c9f
RobJacobs <rob.jacobs@tylertech.com>
2025-11-10 11:08:54 -0500

    chore: publish demo

---
+++ docs/chunk-RS6C2WRR.js
@@ -0,0 +1,2 @@
+import{fa as o}from"./chunk-UQ3J2UFU.js";var n=new o("PetsService"),p=[{path:"",loadComponent:()=>import("./chunk-4NHRO73G.js").then(t=>t.PetsMainComponent)}];export{n as a,p as b};
+//# sourceMappingURL=chunk-RS6C2WRR.js.map
